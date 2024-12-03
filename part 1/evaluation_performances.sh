6d883b4c18858e7460ace55efff3a3b8e55b75e0
Moers Simon <simon.moers@student.uclouvain.be>
2024-12-03 18:09:40 +0100

    tache 1.4: implémentation1

--- part 1/evaluation_performances.sh
+++ part 1/evaluation_performances.sh
@@ -5,3 +5,52 @@
 # o Prendre 5 mesures pour chaque configuration avec les nombres de threads TOTAUX
 # suivants: [2, 4, 8, 16, 32]
 # Pour les problèmes avec deux types distincts de threads, vous devez donc séparer ce nombre en 2 ensembles de threads de taille égale.
+#
+#!/bin/bash
+
+exec 1>/dev/null
+mkdir -p resultats
+
+test() {
+    local program=$1
+    local threads=$2
+    local output_file=$3
+
+    # csv
+    echo "Threads,Temps moyen (s)" > "$output_file"
+
+    for t in $threads; do
+        total_time=0
+
+        # Cas ou deux types de threads
+        if [[ "$program" == "producteurs_consommateurs" || "$program" == "lecteurs_ecrivains" ]]; then
+            half_t=$((t / 2))  # consignes: Diviser le nombre total de threads en deux groupes égaux
+            for i in {1..5}; do
+                start=$(LC_NUMERIC=C date +%s.%N)
+                ./$program $half_t $half_t
+                end=$(LC_NUMERIC=C date +%s.%N)
+                elapsed=$(echo "$end - $start" | bc -l)  # temps écoulé
+                total_time=$(echo "$total_time + $elapsed" | bc -l)
+            done
+        else
+            for i in {1..5}; do
+                start=$(LC_NUMERIC=C date +%s.%N)
+                ./$program $t
+                end=$(LC_NUMERIC=C date +%s.%N)
+                elapsed=$(echo "$end - $start" | bc -l)  # temps écoulé
+                total_time=$(echo "$total_time + $elapsed" | bc -l)
+            done
+        fi
+
+        # Calculer du temps, 3 décimales
+        avg_time=$(echo "$total_time / 5" | bc -l)
+        formatted_time=$(LC_NUMERIC=C printf "%.3f" "$avg_time")
+
+        # Ajouter au csv
+        echo "$t,$formatted_time" >> "$output_file"
+    done
+}
+
+test "philosophes" "2 4 8 16 32" "resultats/philosophes.csv"
+test "lecteurs_ecrivains" "2 4 8 16 32" "resultats/lecteurs_ecrivains.csv"
+test "producteurs_consommateurs" "2 4 8 16 32" "resultats/producteurs_consommateurs.csv"
