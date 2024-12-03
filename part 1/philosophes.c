33b8ca3056681dd8b7819a46a0d1a45d0cd2591b
Moers Simon <simon.moers@student.uclouvain.be>
2024-12-03 18:21:03 +0100

    philosophes: comment

--- part 1/philosophes.c
+++ part 1/philosophes.c
@@ -39,37 +39,43 @@ void* philosophe (void *arg) {
 }
 
 int main(int argc, char *argv[]) {
+    // arguments
     if (argc != 2) {
-        fprintf(stderr, "Usage: %s <nombre_de_philosophes>\n", argv[0]);
+        fprintf(stderr, "Utilisation: %s <nombre_de_philosophes>\n", argv[0]);
         return 1;
     }
     N = atoi(argv[1]);
     if (N <= 1) {
-        fprintf(stderr, "Le nombre de philosophes doit être supérieur à 1.\n");
+        fprintf(stderr, "Nombre de philosophes doit être > 1.\n");
         return 1;
     }
 
+    // tableau threads pour philosophes et tableau d'id
     pthread_t philosophes[N];
     int ids[N];
+
     baguettes = malloc(N * sizeof(pthread_mutex_t));
 
+    // mutex pour les baguettes
     for (int i = 0; i < N; i++) {
         pthread_mutex_init(&baguettes[i], NULL);
     }
 
+    // création thread pour philosophes
     for (int i = 0; i < N; i++) {
         ids[i] = i;
         pthread_create(&philosophes[i], NULL, philosophe, &ids[i]);
     }
 
+    // fin thread philosophes
     for (int i = 0; i < N; i++) {
         pthread_join(philosophes[i], NULL);
     }
-
+    // "destruction" mutex
     for (int i = 0; i < N; i++) {
         pthread_mutex_destroy(&baguettes[i]);
     }
-
+    // libere mémoire allouée
     free(baguettes);
     return 0;
 }
