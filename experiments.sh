cc6415610ba3804e456f02ffdd58987dd5c0ba49
Moers Simon <simon.moers@student.uclouvain.be>
2024-12-04 16:30:04 +0100

    display results

--- experiments.sh
+++ experiments.sh
@@ -5,3 +5,12 @@ make
 
 # Lancer les tests de performance (peut-être une cible spécifique du Makefile, comme 'make test')
 make test
+
+echo "=== Résultats Philosophes ==="
+cat part1/resultats/philosophes.csv
+
+echo "=== Résultats Lecteurs-Écrivains ==="
+cat part1/resultats/lecteurs_ecrivains.csv
+
+echo "=== Résultats Producteurs-Consommateurs ==="
+cat part1/resultats/producteurs_consommateurs.csv
